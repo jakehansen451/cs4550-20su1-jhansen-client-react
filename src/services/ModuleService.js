b60aa63d6c2e0dae38bdc41ba57f05214f099afc
Jake Hansen <hansen.j@husky.neu.edu>
2020-06-06 13:49:16 -0400

    Adds lesson service and updates URLs

--- src/services/ModuleService.js
+++ src/services/ModuleService.js
@@ -1,45 +1,36 @@
-const findAllModules = () => {
-  return fetch("https://wbdv-generic-server.herokuapp.com/api/jannunzi/modules")
+export const findAllModules = () => {
+  return fetch("https://wbdv-generic-server.herokuapp.com/api/jhansen/modules")
   .then(response => response.json())
-}
+};
 
-const findModuleForCourse = (courseId) =>
-    fetch(`https://wbdv-generic-server.herokuapp.com/api/jannunzi/courses/${courseId}/modules`)
-    .then(response => response.json())
+export const findModuleForCourse = (courseId) =>
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/jhansen/courses/${courseId}/modules`)
+    .then(response => response.json());
 
 
-const deleteModule = (moduleId) => {
-  return fetch("https://wbdv-generic-server.herokuapp.com/api/jannunzi/modules/" + moduleId, {
+export const deleteModule = (moduleId) => {
+  return fetch("https://wbdv-generic-server.herokuapp.com/api/jhansen/modules/" + moduleId, {
     method: 'DELETE'
   })
   .then(response => response.json())
-}
+};
 
-const updateModule = (moduleId, newModule) =>
-    fetch(`https://wbdv-generic-server.herokuapp.com/api/jannunzi/modules/${moduleId}`, {
+export const updateModule = (moduleId, newModule) =>
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/jhansen/modules/${moduleId}`, {
       method: 'PUT',
       body: JSON.stringify(newModule),
       headers: {
         'content-type': 'application/json'
       }
     })
-    .then(response => response.json())
+    .then(response => response.json());
 
-const createModule = (courseId, module) =>
-    fetch(`https://wbdv-generic-server.herokuapp.com/api/jannunzi/courses/${courseId}/modules`, {
+export const createModule = (courseId, module) =>
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/jhansen/courses/${courseId}/modules`, {
       method: 'POST',
       body: JSON.stringify(module),
       headers: {
         'content-type': 'application/json'
       }
     })
-    .then(response => response.json())
-
-
-export default {
-  findAllModules,
-  deleteModule,
-  createModule,
-  updateModule,
-  findModuleForCourse
-}
+    .then(response => response.json());
